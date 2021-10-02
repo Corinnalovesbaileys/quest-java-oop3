4df5e9a8ac6432aa8f2ff6564ca8a250f0f201f9
Corinnalovesbaileys <corinna-grimm_student2021@wilder.school>
2021-10-02 15:20:25 +0200

    Aufgabe zur Challenge 08 Interfaces

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,34 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(this.getName() + " auf geht`s! ");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (meters > 100)
+        System.out.println("Uiiiiiii es geht nun " + meters + " m in die Höhe!");
+        else System.out.println(meters + " m sind ja nicht gerade viel.");
+        return meters;
+    }
+
+    @Override
+    public int descend(int meters) {
+        System.out.println("Nun wird es Zeit wieder Land zu sehen, es geht nun " + meters + " m runter." );
+        return meters;
+    }
+
+    @Override
+    public void land() {
+        System.out.println(this.getName() + " nun wird es Zeit die Krallen zu schärfen.");
+    }
+
+    @Override
+    public void glide() {
+        Fly.super.glide();
     }
 }
